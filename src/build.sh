ea6fb679e1d3441593c0a59fa8fb0ff41369a66b
Terren Peterson <terrenjpeterson@yahoo.com>
2017-10-09 00:02:44 +0000

    broke out drinks into separate file

--- src/build.sh
+++ src/build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 # create build package for Lex chatbot, stage in s3 bucket, and deploy package.
 
-# create zip file with all the json data objects
-zip foodbot.zip lambda.js foods.json
+# create temp zip file with all the json data objects
+zip foodbot.zip lambda.js foods.json drinks.json
 
-# copy the files to a staging bucket in case need for research
+# copy the build file and source files to a staging bucket in case need for research
 aws s3 cp lambda.js s3://fastfoodchatbot/binaries/
 aws s3 cp foodbot.zip s3://fastfoodchatbot/binaries/
 aws s3 cp foods.json s3://fastfoodchatbot/binaries/
+aws s3 cp drinks.json s3://fastfoodchatbot/binaries/
 
 # cleanup temporary file
 echo 'removed temp file'
@@ -18,4 +19,3 @@ aws lambda update-function-code --function-name myCalorieCounterGreen --s3-bucke
 
 # wrap-up
 echo 'completed new deployment'
-#myCalorieCounterGreen
