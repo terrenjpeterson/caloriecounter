09de1df444d1e8392c1ee16c455cc549c51bea5f
Terren Peterson <terrenjpeterson@yahoo.com>
2017-11-11 03:35:58 +0000

    added new function to build process

--- src/build.sh
+++ src/build.sh
@@ -1,31 +1,40 @@
 #!/bin/bash
-# create build package for Lex chatbot, stage in s3 bucket, and deploy package.
+# create build package for Lex chatbot, stage in s3 bucket, deploy package, then run a few tests to validate deployment
 
 # create temp zip file with all the json data objects
-zip -r foodbot.zip lambda.js data/foods.json data/drinks.json package.json
+zip -r foodbot.zip lambda.js data/restaurants.json data/foods.json data/drinks.json package.json
+zip -r calcbot.zip calculate.js data/restaurants.json data/foods.json data/drinks.json
 zip -r pizzabot.zip pizza.js data/pizzas.json
 
 # copy the build file and source files to a staging bucket in case need for research
 aws s3 cp lambda.js s3://fastfoodchatbot/binaries/
 aws s3 cp pizza.js s3://fastfoodchatbot/binaries/
+aws s3 cp calculate.js s3://fastfoodchatbot/binaries/
 aws s3 cp foodbot.zip s3://fastfoodchatbot/binaries/
 aws s3 cp pizzabot.zip s3://fastfoodchatbot/binaries/
+aws s3 cp calcbot.zip s3://fastfoodchatbot/binaries/
+echo 'copied zip files and source code to s3'
 
 aws s3 cp data/foods.json s3://fastfoodchatbot/data/
 aws s3 cp data/drinks.json s3://fastfoodchatbot/data/
 aws s3 cp data/pizzas.json s3://fastfoodchatbot/data/
+aws s3 cp data/restaurants.json s3://fastfoodchatbot/data/
+echo 'copied data files to s3'
 
-# cleanup temporary file
-echo 'removed temp files'
+# cleanup temporary zip files
 rm foodbot.zip
 rm pizzabot.zip
+rm calcbot.zip
+echo 'removed temp files'
 
 # update the lambda function with the binaries that have been staged
 aws lambda update-function-code --function-name myCalorieCounterGreen --s3-bucket fastfoodchatbot --s3-key binaries/foodbot.zip
 aws lambda update-function-code --function-name myPizzaCalorieCounterGreen --s3-bucket fastfoodchatbot --s3-key binaries/pizzabot.zip
-echo 'new version has been deployed'
+aws lambda update-function-code --function-name myCalorieCalculatorGreen --s3-bucket fastfoodchatbot --s3-key binaries/calcbot.zip
+echo 'new versions of lambda functions has been deployed'
 
-# read in test data required for a basic food request
+# read in test data required for a food validation request
+echo 'test case 1 - food validation'
 cd testing
 request=$(<request.json)
 cd ..
@@ -36,8 +45,24 @@ aws lambda invoke --function-name myCalorieCounterGreen --payload "$request" tes
 # read response file into local variable then print on the console
 response=$(<testOutput.json)
 echo $response
+echo 'test case 1 complete'
 
-# read in test data required for the second request
+# read in test data required for a food calculation request
+echo 'test case 2 - food calculation'
+cd testing
+request=$(<calcRequest.json)
+cd ..
+
+# invoke the new lambda function
+aws lambda invoke --function-name myCalorieCalculatorGreen --payload "$request" testOutput.json
+
+# read response file into local variable then print on the console
+response=$(<testOutput.json)
+echo $response
+echo 'test case 2 complete'
+
+# read in test data required for the pizza test
+echo 'test case 3 - pizza request'
 cd testing
 request=$(<pizzaRequest.json)
 cd ..
@@ -48,6 +73,7 @@ aws lambda invoke --function-name myPizzaCalorieCounterGreen --payload "$request
 # read response file into local variable then print on the console
 response=$(<testOutput.json)
 echo $response
+echo 'test case 3 complete'
 
 # clean-up any temporary data
 #aws s3 rm foodbot.zip s3://fastfoodchatbot/binaries/
