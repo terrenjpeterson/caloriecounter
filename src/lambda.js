e2d5c8faf88711509404824883fc317dbb988afb
Terren Peterson <terrenjpeterson@yahoo.com>
2017-11-12 20:35:47 +0000

    removed code moved to other functions

--- src/lambda.js
+++ src/lambda.js
@@ -584,69 +584,6 @@ function validateMexicanFood(intentRequest) {
     }
 }
 
-// this function is what builds the introduction
-
-function getIntroduction(intentRequest, callback) {
-    const sessionAttributes = intentRequest.sessionAttributes || {};
-    const slots = intentRequest.currentIntent.slots;
-
-    var counterResponse = 'Hello. I am a chatbot that can assist you in calculating ' +
-        'calories for different fast food restaurants. To get started, please say ' +
-        'something like How many calories in a Big Mac, and I will do all the work!';
-
-    callback(close(sessionAttributes, 'Fulfilled',
-        { contentType: 'PlainText', content: counterResponse }));
-
-}
-
-// this function is what retrieves the restaurants that data is available for
-
-function getRestaurants(intentRequest, callback) {
-    const sessionAttributes = intentRequest.sessionAttributes || {};
-
-    var counterResponse = 'Here are the fast food restaurants that I have ' +
-        'information on. ';
-
-    for (var i = 0; i < restaurants.length; i++) {
-        counterResponse = counterResponse + restaurants[i] + ', ';
-    }
-
-	counterResponse = counterResponse + 'Say something like, eating at McDonalds, to begin.';
-
-    callback(close(sessionAttributes, 'Fulfilled',
-        { contentType: 'PlainText', content: counterResponse }));
-        
-}
-
-// this function is what builds the wrap-up of a conversation
-
-function endConversation(intentRequest, callback) {
-    const sessionAttributes = intentRequest.sessionAttributes || {};
-
-    var counterResponse = 'Thanks for checking in. Have a nice day! ';
-
-    callback(close(sessionAttributes, 'Fulfilled',
-        { contentType: 'PlainText', content: counterResponse }));
-        
-}
-
-// this function is what builds the response to a request for help
-
-function getHelp(intentRequest, callback) {
-    const sessionAttributes = intentRequest.sessionAttributes || {};
-
-    var counterResponse = 'This is the Fast Food Calorie Checker chatbot. ' +
-        'I am a resource to reference how many calories are in different fast foods. ' +
-        'To get started, just say something like How many calories in a Big Mac, ' +
-	'or Eating one slice of Peperroni Pizza, and I will ask a few additional ' +
-        'questions and calculate the amount for you. For the latest list of fast food ' +
-        'restaurants I know about, just say List of restaurants.';
-
-    callback(close(sessionAttributes, 'Fulfilled',
-        { contentType: 'PlainText', content: counterResponse }));
-        
-}
-
 function getMealDetails(intentRequest, callback) {
     const sessionAttributes = intentRequest.sessionAttributes || {};
 
@@ -675,68 +612,6 @@ function getMealDetails(intentRequest, callback) {
 
     callback(close(sessionAttributes, 'Fulfilled',
 	{ contentType: 'PlainText', content: detailResponse }));
-
-}
-
-// this function retrieves the food options for a given restaurant
-function getFoodOptions(intentRequest, callback) {
-    const sessionAttributes = intentRequest.sessionAttributes || {};
-
-    var restaurant = intentRequest.currentIntent.slots.Restaurant;
-    var foodType   = intentRequest.currentIntent.slots.FoodType;
-
-    var botResponse = "Here are the types of " + foodType + " at " + restaurant + ". ";
-
-    console.log("Attempting to retrieve types of " + foodType + " at " + restaurant + ".");
-
-    // clean up different spellings of food types - including plural items
-    if (foodType.toLowerCase() === "burritos") {
-	foodType = "Burrito";
-    } else if (foodType.toLowerCase() === "salads") {
-	foodType = "Salad";
-	console.log("corrected Salads to Salad for food lookup");
-    } else if (foodType.toLowerCase() === "chalupas") {
-	foodType = "Chalupa";
-    } else if (foodType.toLowerCase() === "sandwiches") {
-	foodType = "Sandwich";
-    } else if (foodType.toLowerCase() === "burgers") {
-	foodType = "Burger";
-    }
-
-    var foodItems = [];
-    // find the restaurant food items for the restaurant provided
-    for (var i = 0; i < foodChoices.length; i++) {
-        if (restaurant.toLowerCase() === foodChoices[i].restaurant.toLowerCase()) {
-            foodItems = foodChoices[i].foodItems;
-	    restaurant = foodChoices[i].restaurant;
-        } 
-    }
-
-    // go through the food items, and list those matching the food type
-    var foodTypeMatch = false;
-    var foodNameExample = "";
-
-    for (var j = 0; j < foodItems.length; j++) {
-	// first make sure a food type exists for the item
-	if (foodItems[j].foodType) {
-	    if (foodItems[j].foodType.toLowerCase() === foodType.toLowerCase()) {
-		botResponse = botResponse + foodItems[j].foodName + ", ";
-		foodNameExample = foodItems[j].foodName;
-		foodTypeMatch = true;
-	    }
-	}
-    }
-
-    if (foodTypeMatch) {
-	botResponse = botResponse + " Want calorie details? Say something like " +
-	    "How many calories in a " + foodNameExample + " at " + restaurant + ".";
-    } else {
-	botResponse = "Sorry, I don't have information for types of " + foodType + " at " +
-	    restaurant + ".";
-    }
-
-    callback(close(sessionAttributes, 'Fulfilled',
-        { contentType: 'PlainText', content: botResponse }));
 
 }
 
@@ -880,27 +755,9 @@ function dispatch(intentRequest, callback) {
     if (intentRequest.invocationSource === 'DialogCodeHook') {
         console.log("Validation in progress.");
         validateUserEntry(intentRequest, callback);
-    } else if (intentName === 'Introduction') {
-        console.log("friendly introduction.");
-        return getIntroduction(intentRequest, callback);
     } else if (intentName === 'WhichRestaurants') {
         console.log("list of restaurants requested.");
         return getRestaurants(intentRequest, callback);
-    } else if (intentName === 'EndConversation') {
-        console.log("wrap-up conversation requested.");
-        return endConversation(intentRequest, callback);
-    } else if (intentName === 'Thanks') {
-        console.log("received thank you from user.");
-        return endConversation(intentRequest, callback);
-    } else if (intentName === 'HelpRequest') {
-        console.log("user requested help.");
-        return getHelp(intentRequest, callback);
-    } else if (intentName === 'FoodTypeOptions') {
-	console.log("user requested food types");
-	return getFoodOptions(intentRequest, callback);
-    } else if (intentName === 'MoreDetails') {
-	console.log("user requested details on meal");
-	return getMealDetails(intentRequest, callback);
     }
     
     throw new Error(`Intent with name ${intentName} not supported`);
